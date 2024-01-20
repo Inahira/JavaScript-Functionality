3d654562076e3441e4d92e40efb76118ad656f5d
Tahira inam <sololearner120@gmai.com>
2024-01-20 09:53:11 -0800

    color switch

---
+++ BG-color/indeks.js
@@ -0,0 +1,13 @@
+let body = document.querySelector('body');
+document.querySelector('#btn1').addEventListener('click', function (){
+    body.style.backgroundColor = 'red';
+})
+document.querySelector('#btn2').addEventListener('click', function (){
+    body.style.backgroundColor = 'yellow';
+})
+document.querySelector('#btn3').addEventListener('click', function (){
+    body.style.backgroundColor = 'green';
+})
+document.querySelector('#btn4').addEventListener('click', function (){
+    body.style.backgroundColor = 'blue';
+})
